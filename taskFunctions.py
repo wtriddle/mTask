7f942adf94f7ffbd9bf2c81e0ff2df1456ecc488
William Riddle <wtriddle31@gmail.com>
2020-05-29 11:42:10 -0700

    Edit task functionality added

--- taskFunctions.py
+++ taskFunctions.py
@@ -80,4 +80,51 @@ class TaskFunctions():
         taskTime = rec['taskTime']
         self.mTask.addTaskToGUI(**{'taskName': taskName, 'taskTime' : taskTime, 'routineName' : routineName})
     def editTask(self):
-        print("Editing task...")
+        # Load userTasks from database
+        self.mTask.mTaskDB.set_table(tablename = "Tasks")
+        recs = list(self.mTask.mTaskDB.getrecs())
+        userTasks = [rec['taskName'] for rec in recs]
+
+        # Form Window ------------------------------------------------------
+        window = Toplevel(self.mTask.root)
+        containerFrame = ttk.Frame(window)
+        containerFrame.pack(expand = True, fill = "both", padx = 50, pady = 50)
+
+        ttk.Label(containerFrame, text = "Choose a Task to Change").grid(row = 0, column = 0, pady = 10, padx = 10)
+
+        self.taskEntry = ttk.Combobox(containerFrame, state = "readonly")
+        self.taskEntry.bind("<<ComboboxSelected>>", self.fillEntries)
+        self.taskEntry.config(values = userTasks)
+        self.taskEntry.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+        ttk.Label(containerFrame, text = "Enter a new Task Name").grid(row = 2, column = 0, pady = 10, padx = 10)
+
+        self.newTaskEntry = ttk.Entry(containerFrame)
+        self.newTaskEntry.grid(row = 3, column = 0, padx = 10, pady = 10)
+
+        ttk.Label(containerFrame, text = "Enter a new Task Start Time").grid(row = 4, column = 0, pady = 10, padx = 10)
+
+        self.newTimeEntry = ttk.Entry(containerFrame)
+        self.newTimeEntry.grid(row = 5, column = 0, padx = 10, pady = 10)
+
+        self.submitButton = ttk.Button(containerFrame, text = "Edit", command = self.submitEditTaskData)
+        self.submitButton.grid(row = 6, column = 0, pady = 20, sticky = NSEW)
+        # ~ Form Window ------------------------------------------------------
+    def fillEntries(self, event):
+        taskName = str(self.taskEntry.get())
+        query = f'SELECT * FROM Tasks WHERE taskName = \"{taskName}\"'
+        rec = dict(self.mTask.mTaskDB.sql_query_row(query))
+        self.newTaskEntry.delete(0, END)
+        self.newTimeEntry.delete(0, END)
+        self.newTaskEntry.insert(0,rec['taskName'])
+        self.newTimeEntry.insert(0,rec['taskTime'])
+    def submitEditTaskData(self):
+        taskName = self.taskEntry.get()
+        if not taskName:
+            mBox.showerror(title = "Task Edit Error", message= "Please choose task to edit")
+            return 
+        newTaskName = self.newTaskEntry.get()
+        newTime = self.newTimeEntry.get()
+        query = f'UPDATE Tasks SET taskName = \"{newTaskName}\", taskTime = \"{newTime}\" WHERE taskName = \"{taskName}\"'
+        self.mTask.mTaskDB.sql_do(query)
+        mBox.showinfo(title = "Sucess!", message= "Your task had been updated")
